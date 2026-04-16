5927811cebb2d2fc3ce8f9cbe2e402f0e9859629
Manjot Bhoot <msb_01@outlook.com>
2026-04-16 17:04:03 +0100

    added clarity analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,16 @@ export default function RootLayout({
           `}
         </Script>
 
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wcneusp32l");
+          `}
+        </Script>
+
         <Providers>{children}</Providers>
       </body>
     </html>
